7263d99076fa6189c0d0eb7dc29a4c551d0874b2
Mohammad Ryan Ur Rafi <ryan.rafi@therapservices.net>
2026-09-16 11:40:27 +0600

    add dev config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   apps: [
     {
-      name: "mokushi-backend",
+      name: process.env.PM2_NAME || "mokushi-backend",
       script: "dist/src/main.js",
-      instances: 2,
-      exec_mode: "cluster",
+      instances: process.env.NODE_ENV === "production" ? 2 : 1,
+      exec_mode: process.env.NODE_ENV === "production" ? "cluster" : "fork",
       watch: false,
       max_memory_restart: "300M",
       env: {
-        NODE_ENV: "production",
+        NODE_ENV: process.env.NODE_ENV || "production",
       },
     },
   ],
